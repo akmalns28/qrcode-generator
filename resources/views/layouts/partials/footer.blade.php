6475761b1c58abd1672f3f4d5f38fb1b5fd76bbd
AkmalNS <akmalnursidiq10@gmail.com>
2026-07-08 08:26:23 +0700

    feat: finish

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,11 +2,10 @@
     <div class="container">
         <div class="footer clearfix mb-0 text-muted">
             <div class="float-start">
-                <p>2026 &copy; ANS</p>
+                <p>2026 &copy; Akmal Nur Sidiq</p>
             </div>
             <div class="float-end">
-                <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="#">Akmal</a>
-                </p>
+                
             </div>
         </div>
     </div>
